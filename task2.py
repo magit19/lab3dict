bae6c9479604410379dae2672f312e767f75f583
Alexey Koryakin <alexey@koryakin.ua>
2019-10-24 19:17:15 +0900

    task2 nedokonchen

---
+++ task2.py
@@ -0,0 +1,13 @@
+def last_digit(L):
+	d = {}
+	for i in range (0,len(s),1):
+		d[L[x%10]] = L[] 
+
+	return d
+
+
+if __name__ == '__main__':
+	print("Введите числа:")
+	s = input().split()
+	L = [int(x) for x in s]
+	print(last_digit(s))
