c2380b3b5975d0d8a097ffc9ff3920defb814ebb
Aleksandr Pavlov <askhgfasdfg@noreply.github.com>
2019-10-24 19:11:45 +0900

    Болванка задачи2

---
+++ task2.py
@@ -0,0 +1,12 @@
+def last_digit(L):
+	d = {}
+	# подсчитать последние цифры всех чисел
+	return d
+
+
+if __name__ == '__main__':
+	print("Введите числа:")
+	s = input().split()
+	L = [int(x) for x in s]	
+	print(last_digit(L))
+
