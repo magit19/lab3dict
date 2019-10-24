3d3cb95443b36c88a97a3fbcfb55c83bfca9f95a
Nikiforov Bair <NikiforovBair@gmail.com>
2019-10-24 19:00:55 +0900

    первая задача

--- task1.py
+++ task1.py
@@ -1,9 +1,8 @@
 def make_dict(L):
 	d = {}
-	# .... сделать из списка словарь
+	for i in range(0,len(L),2):
+		d[L[i]]= L[i+1]
 	return d
-
-
 if __name__ == '__main__':
 	print("Введите четное количество слов:")
 	s = input().split()
