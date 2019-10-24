aa1a8b51b3fbb6127659f1f3b959eaa52a8e15fb
ВАШЕИМЯ <yourmail@gmail.com>
2019-10-24 18:01:52 +0900

    Болванка задачи1

---
+++ task1.py
@@ -0,0 +1,11 @@
+def make_dict(L):
+	d = {}
+	# .... сделать из списка словарь
+	return d
+
+
+if __name__ == '__main__':
+	print("Введите четное количество слов:")
+	s = input().split()
+	print(make_dict(s))
+
