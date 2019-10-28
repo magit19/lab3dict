efde21dcb52afe357b4db5fcdc60bdf733618e7c
neustroev <ea.neustroev@stud.s-vfu.ru>
2019-10-28 17:08:17 +0900

    2 onordum bihilax

--- task2.py
+++ task2.py
@@ -1,13 +1,18 @@
+from collections import defaultdict
 def last_digit(L):
-	d = {}
-	for i in range(0, len(L), 1):
-		d[L[i]] = L[i%10]
+	d = defaultdict(int)
+	
+	for x in L:
+		last = x%10
+		d[last] += 1
 	return d
+
+
 
 
 if __name__ == '__main__':
 	print("Введите числа:")
 	s = input().split()
-	L = [int(x) for x in s]
-	print(last_digit(s))
+	L = [int(x) for x in s]	
+	print(last_digit(L))
 
