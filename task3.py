ff6e8de0c38d33162df43ea457a839a895dedde1
Aleksandr Pavlov <askhgfasdfg@noreply.github.com>
2019-10-28 17:23:40 +0900

    болванка 3

---
+++ task3.py
@@ -0,0 +1,18 @@
+d = {}
+
+N = int(input())
+for i in range(N):
+	line = input().split()
+	fn = line[0]
+	d[fn] = ?????
+
+
+M = int(input())
+for i in range(M):
+	action, fn = input().split()
+	if ?????:
+		print("OK")
+	else:
+		print("Acces denied")
+
+print(d)
