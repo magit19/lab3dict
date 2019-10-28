58440d023e830603ed3616d454ef85b35e505e5c
Nikiforov Bair <NikiforovBair@gmail.com>
2019-10-28 16:14:35 +0900

    MaxCorp предоставляет эффектно выполненную задачу 1

--- task1.py
+++ task1.py
@@ -1,5 +1,6 @@
 def make_dict(L):
 	d = {}
+	d = {L[i]:L[i+1] for i in range(0,len(L),2)}
 	# .... сделать из списка словарь
 	return d
 
@@ -8,4 +9,3 @@ if __name__ == '__main__':
 	print("Введите четное количество слов:")
 	s = input().split()
 	print(make_dict(s))
-
