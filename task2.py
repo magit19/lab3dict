f3df7f9d27a48c26872117985bf0a85ccaa371db
Baibolot Ajybek Uulu <baibolot@fgfgf.ghhg>
2019-10-28 17:16:42 +0900

    reshil #2

--- task2.py
+++ task2.py
@@ -1,12 +1,13 @@
-def last_digit(L):
-	d = {}
-	# подсчитать последние цифры всех чисел
+from collections import defaultdict
+def last_dict(L):
+	d = defaultdict(int)
+	for x in L:
+		last = x%10
+		d[last] += 1
 	return d
-
 
 if __name__ == '__main__':
 	print("Введите числа:")
 	s = input().split()
-	L = [int(x) for x in s]	
-	print(last_digit(L))
-
+	L = [int(x) for x in s]
+	print(last_dict(L))
