a8d0b885052dcc3a689c895a06ad43865e706ce1
Alexey Koryakin <narutouzumaki@mail.konoha>
2019-12-11 14:57:32 +0900

    task2 done

--- task2.py
+++ task2.py
@@ -1,7 +1,6 @@
 def last_digit(L):
 	import collections
-
-	d = {x:s.count(x) for x in L}
+	d = {x: a.count(x) for x in a}
 
 	return d
 
@@ -9,5 +8,6 @@ if __name__ == '__main__':
 	print("Введите числа:")
 	s = input().split()
 	L = [int(x) for x in s]
+	a = [x%10 for x in L]
 
 	print(last_digit(s))
