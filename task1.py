8c50ce9bc9c71a89a1d9ed0d0416ba87a27eeea8
Dmitry <skryabind98@gmail.com>
2019-12-25 21:38:41 +0900

    ya hochu 5

--- task1.py
+++ task1.py
@@ -6,5 +6,5 @@ def make_dict(L):
 
 if __name__ == '__main__':
 	print("Введите четное количество слов в формате [ключ1, значение1, ключ2, значение2,...]:")
-	s = input().split()
+	s = input().split() 
 	print(make_dict(s))
