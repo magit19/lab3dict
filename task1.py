cca6abc62bbb696c2d9f2230bead8043b243f5ab
DenisovArt <DenisovArt@nime.com>
2020-01-05 16:58:28 +0900

    Ispravil task1

--- task1.py
+++ task1.py
@@ -1,13 +1,10 @@
 def make_dict(L):
 	d = {}
 	for i in range(0, len(s), 2):
-			d[L[i]] = L[i+1]
+		d[L[i]] = L[i+1]
 
 	return d
 
-
 if __name__ == '__main__':
-	print("Введите четное количество слов:")
-	s = input().split()
+	s = input('Введите четное число слов\n').split()
 	print(make_dict(s))
-
